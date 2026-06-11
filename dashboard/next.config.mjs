db7d4c67577be397f375ec2c123e25bed835231e
zinaleeHQ <zina@otmexpert.com>
2026-06-11 11:01:54 -0600

    Update next.config.mjs
    clean code switch

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   basePath: '/project-vista/dashboard',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
